001ac372349f8c66181695eb8dddafa1ca924014
SebasBarrientos <sebas33696@gmail.com>
2024-04-14 03:45:05 +0200

    Grandes modificaciones al proyecto, ahora es sobre todos los pokemon de primera generacion- implementadas modificaciones de recoleccion de datos de la api y asignada visual de next

--- main.js
+++ main.js
@@ -7,50 +7,39 @@ const pictures = document.getElementById("pictures");
 
 let currentQuestionIndex;
 
-//<img class="active" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png" alt="Bulbasaur">
 const game = () => {
-  let numero = Math.floor(Math.random() * 12 + 1);
-  let numeroFormateado = numero.toString().padStart(3, "0"); //padStart rellena la cadena con "0" si el lenght es menor a 3
-  let idPokemonSearched = Math.ceil(numero / 3);
-  const fotoPokemon = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${numeroFormateado}.png`;
-
+  const number = () => {
+    let number = Math.floor(Math.random() * 151 + 1);
+    return number;
+  };
   let chainPokemones = [];
   let rtaCorrecta;
+  let pokemon = [];
+  console.log(number());
 
-  const respuestaCorrecta = () => {
-    axios
-      .get(`https://pokeapi.co/api/v2/pokemon/${numero}`)
-      .then((res) => {
-        rtaCorrecta = res.data.name;
-        // let rtaCorrectaID = res.data.id;
+  function backGroundColorAns(button) {
+    if (button.dataset.correct == "true") {
+      button.classList.add("bg-success");
+    } else {
+      button.classList.add("bg-danger");
+    }
+  }
+  function selectAnswer(answerSelected) {
+    Array.from(answerButtonsElement.children).forEach((button) => {
+      backGroundColorAns(button);
+    });
+    console.log(answerSelected);
+    if (answerSelected != "") {
+      let puntuacion = Number(localStorage.getItem("Puntuacion"));
+      console.log(typeof puntuacion);
+      puntuacion += 1;
+      localStorage.setItem("Puntuacion", puntuacion);
+      nextButton.classList.remove("d-none");
 
-        console.log("Respuesta Correcta", rtaCorrecta);
-      })
-      .catch((err) => console.error(err));
-  };
-  respuestaCorrecta();
-  
-  const respuestasBtn = () => {
-    axios
-      .get(`https://pokeapi.co/api/v2/evolution-chain/${idPokemonSearched}`)
-      .then((res) => {
-        let pokemon1 = res.data.chain.species.name;
-        let pokemon2 = res.data.chain.evolves_to[0].species.name;
-        let pokemon3 = res.data.chain.evolves_to[0].evolves_to[0].species.name;
-        chainPokemones.push(pokemon1, pokemon2, pokemon3);
-        answersButtons(chainPokemones, rtaCorrecta);
-        console.log(pokemon1);
-        console.log(pokemon2);
-        console.log(pokemon3);
-      })
-      .catch((err) => console.error(err));
-  };
-  respuestasBtn();
-  console.log(chainPokemones);
-
-  //Hay que resolver como le paso el valor seleccionado
-  //Me quede en answer. Estoy tratando de que aparezcan los botones
-  function answersButtons(chainPokemones, rtaCorrecta) {
+      console.log("SUMASTE UN PUNTO!");
+    }
+  }
+  const answersButtons = (chainPokemones, rtaCorrecta) => {
     console.log(chainPokemones);
     console.log("ANS ", rtaCorrecta);
     //Ver como conecto el dom y los botones aca!!
@@ -69,56 +58,64 @@ const game = () => {
       });
       answerButtonsElement.appendChild(button);
     });
+  };
+  const pokemonCorrect = (pokemon) => {
+    let numeroPokemon = number();
+    let pokemonCorrect = pokemon[numeroPokemon - 1].name;
+    chainPokemones.push(pokemonCorrect);
+    rtaCorrecta = pokemonCorrect;
+    console.log("Rta Correcta", chainPokemones);
+    console.log("Rta Correcta", rtaCorrecta);
+    console.log(numeroPokemon);
+    let numberFormateado = numeroPokemon.toString().padStart(3, "0"); //padStart rellena la cadena con "0" si el lenght es menor a 3
 
-    function backGroundColorAns(button) {
-      if (button.dataset.correct == "true") {
-        button.classList.add("bg-success");
-      } else {
-        button.classList.add("bg-danger");
-      }
-    }
-    function selectAnswer(answerSelected) {
-      Array.from(answerButtonsElement.children).forEach((button) => {
-        backGroundColorAns(button);
-      });
-      console.log(answerSelected);
-      if (answerSelected != "") {
-        let puntuacion = Number(localStorage.getItem("Puntuacion"));
-        console.log(typeof puntuacion);
-        puntuacion += 1;
-        localStorage.setItem("Puntuacion", puntuacion);
+    const fotoPokemon = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${numberFormateado}.png`;
+    console.log(fotoPokemon);
 
-        console.log("SUMASTE UN PUNTO!");
-      }
-    }
-    // function selectAnswer() {
-    //   Array.from(answerButtonsElement.children).forEach((button) => {
-    //     backGroundColorAns(button);
-    //   });
     pictures.innerHTML = `<img class="active" src="${fotoPokemon}" alt="pokemon">`;
-    pictures.classList.remove("d-none");
-  }
+    pokeFalse(pokemon, numeroPokemon);
+  };
+  const pokeFalse = (pokemon, numeroPokemon) => {
+    let numberPokemonFalse = number();
+    if (numeroPokemon == numberPokemonFalse) {
+      pokeFalse(pokemon, numeroPokemon);
+    } else {
+      let pokemonFalse = pokemon[numberPokemonFalse].name;
+      chainPokemones.push(pokemonFalse);
+      if (chainPokemones.length < 3) {
+        pokeFalse(pokemon, numeroPokemon);
+      } else {
+        answersButtons(chainPokemones, rtaCorrecta);
+      }
+    }
+  };
+
+  const obteniendoPokemons = () => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=151`)
+      .then((res) => {
+        pokemon = res.data.results;
+        console.log(pokemon);
+        pokemonCorrect(pokemon);
+      })
+      .catch((err) => console.error(err));
+  };
+
+  obteniendoPokemons();
+
+  pictures.classList.remove("d-none");
 };
+
 //en el click de next debo hacer un if que si el currentQuestionIndex = 9 se detenga y muestre el resultado
-function startGame() {
+const startGame = () => {
   startButton.classList.add("d-none");
   //   localStorage.setItem("Puntuacion", '0')
   currentQuestionIndex = 0;
   game();
-}
-
-function resetState() {
+};
+const resetState = () => {
   nextButton.classList.add("d-none");
   answerButtonsElement.innerHTML = "";
-}
+};
 
 startButton.addEventListener("click", startGame);
-axios
-    .get(`https://pokeapi.co/api/v2/pokemon`)
-    .then((res) => {
-      let pokemones = res;
-      // let rtaCorrectaID = res.data.id;
-
-      console.log("Respuesta Pokemones", pokemones);
-    })
-    .catch((err) => console.error(err));
