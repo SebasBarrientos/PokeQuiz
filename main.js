4e3d2b5cb8a5e1188d00ac02b0a7db94246bbd6f
SebasBarrientos <sebas33696@gmail.com>
2024-04-14 14:44:53 +0200

    Tornando la pagina en SPA en su totalidad (eliminada la actualizacion constante de la imagen del quiz por 10 imagenes preseteadas)

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const answerButtonsElement = document.getElementById("answer-buttons");
 const pictures = document.getElementById("pictures");
 
 let currentQuestionIndex;
-
+let arrPokemonQuestion = [];
 const game = () => {
   const number = () => {
     let number = Math.floor(Math.random() * 151 + 1);
@@ -31,10 +31,13 @@ const game = () => {
     console.log(answerSelected);
     if (answerSelected != "") {
       let puntuacion = Number(localStorage.getItem("Puntuacion"));
-      console.log(typeof puntuacion);
+      console.log(puntuacion);
       puntuacion += 1;
       localStorage.setItem("Puntuacion", puntuacion);
       nextButton.classList.remove("d-none");
+      //AGREGARLE A LOS BOTONES que queden bloquados!!!
+      //USAR
+      //Array.from(answerButtonsElement.children).forEach((button) => {
 
       console.log("SUMASTE UN PUNTO!");
     }
@@ -42,7 +45,6 @@ const game = () => {
   const answersButtons = (chainPokemones, rtaCorrecta) => {
     console.log(chainPokemones);
     console.log("ANS ", rtaCorrecta);
-    //Ver como conecto el dom y los botones aca!!
     chainPokemones.forEach((pokebutton) => {
       console.log(pokebutton);
       const button = document.createElement("button");
@@ -59,22 +61,7 @@ const game = () => {
       answerButtonsElement.appendChild(button);
     });
   };
-  const pokemonCorrect = (pokemon) => {
-    let numeroPokemon = number();
-    let pokemonCorrect = pokemon[numeroPokemon - 1].name;
-    chainPokemones.push(pokemonCorrect);
-    rtaCorrecta = pokemonCorrect;
-    console.log("Rta Correcta", chainPokemones);
-    console.log("Rta Correcta", rtaCorrecta);
-    console.log(numeroPokemon);
-    let numberFormateado = numeroPokemon.toString().padStart(3, "0"); //padStart rellena la cadena con "0" si el lenght es menor a 3
 
-    const fotoPokemon = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${numberFormateado}.png`;
-    console.log(fotoPokemon);
-
-    pictures.innerHTML = `<img class="active" src="${fotoPokemon}" alt="pokemon">`;
-    pokeFalse(pokemon, numeroPokemon);
-  };
   const pokeFalse = (pokemon, numeroPokemon) => {
     let numberPokemonFalse = number();
     if (numeroPokemon == numberPokemonFalse) {
@@ -83,10 +70,46 @@ const game = () => {
       let pokemonFalse = pokemon[numberPokemonFalse].name;
       chainPokemones.push(pokemonFalse);
       if (chainPokemones.length < 3) {
-        pokeFalse(pokemon, numeroPokemon);
+        pokeFalse(pokemon, numeroPokemon); //COMO HAGO PARA QUE NO SEAN IGUALES LOS 2 FALSOS NOMBRES
       } else {
         answersButtons(chainPokemones, rtaCorrecta);
       }
+    }
+  };
+  const pokemonCorrect = (pokemon, arrPokemonQuestion) => {
+    let numeroPokemon = arrPokemonQuestion[currentQuestionIndex];
+    console.log(pokemon);
+    let pokemonCorrect = pokemon[numeroPokemon - 1].name;
+    chainPokemones.push(pokemonCorrect);
+    rtaCorrecta = pokemonCorrect;
+    console.log("Rta Correcta", chainPokemones);
+    console.log("Rta Correcta", rtaCorrecta);
+    console.log(numeroPokemon);
+
+    // pictures.innerHTML = `<img class="active" src="${fotoPokemon}" alt="pokemon">`;
+    pokeFalse(pokemon, numeroPokemon);
+  };
+  const imgCharge = (arrPokemonQuestion) => {
+    arrPokemonQuestion.forEach((numberImgQuestion) => {
+      imgID = 0;
+      let numberFormateado = numberImgQuestion.toString().padStart(3, "0"); //padStart rellena la cadena con "0" si el lenght es menor a 3
+      const fotoPokemon = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${numberFormateado}.png`;
+      console.log(fotoPokemon);
+      pictures.innerHTML += `<img class="active d-none" id= "img${imgID}" src="${fotoPokemon}" alt="pokemon">`;
+      imgID += 1;
+    });
+    document.getElementById("img0").classList.remove("d-none");
+  };
+
+  const arrPokemonQuestionFiller = (pokemon) => {
+    console.log(arrPokemonQuestion);
+    if (arrPokemonQuestion.length < 10) {
+      arrPokemonQuestion.push(number());
+      arrPokemonQuestionFiller(pokemon);
+    } else {
+      //EJECUTAR CARGA DE IMAGENES
+      imgCharge(arrPokemonQuestion);
+      pokemonCorrect(pokemon, arrPokemonQuestion);
     }
   };
 
@@ -96,7 +119,7 @@ const game = () => {
       .then((res) => {
         pokemon = res.data.results;
         console.log(pokemon);
-        pokemonCorrect(pokemon);
+        arrPokemonQuestionFiller(pokemon);
       })
       .catch((err) => console.error(err));
   };
