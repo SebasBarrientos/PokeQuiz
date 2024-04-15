327906cca7fa2549528fccd00cd2647b80e9364e
SebasBarrientos <sebas33696@gmail.com>
2024-04-15 20:34:38 +0200

    Mejorada interfaz

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const startButton = document.getElementById("start-btn");
 const nextButton = document.getElementById("next-btn");
 const questionContainerElement = document.getElementById("question-container");
-const pokeOptions = document.getElementById("question");
 const answerButtonsElement = document.getElementById("answer-buttons");
 const pictures = document.getElementById("pictures");
 const medals = document.getElementById("medals");
+const quest = document.getElementById("quest");
+
 const linkMedals = [
   "https://static.wikia.nocookie.net/espokemon/images/3/39/Medalla_Roca.png",
   "https://static.wikia.nocookie.net/espokemon/images/6/60/Medalla_Cascada.png",
@@ -29,7 +30,7 @@ let rtaCorrecta;
 let pokemones = [];
 // console.log(number());
 
-function backGroundColorAns(button) {
+const backGroundColorAns = (button)=> {
   button.setAttribute("disabled", "");
   if (button.dataset.correct == "true") {
     button.classList.add("bg-success");
@@ -37,7 +38,7 @@ function backGroundColorAns(button) {
     button.classList.add("bg-danger");
   }
 }
-function selectAnswer(answerSelected) {
+const selectAnswer = (answerSelected) => {
   Array.from(answerButtonsElement.children).forEach((button) => {
     backGroundColorAns(button);
   });
@@ -58,13 +59,9 @@ function selectAnswer(answerSelected) {
       pictures.innerHTML = "";
       startButton.classList.remove("d-none");
     }
-    //AGREGARLE A LOS BOTONES que queden bloquados!!!
-    //USAR
-    //Array.from(answerButtonsElement.children).forEach((button) => {
-
-    // console.log("SUMASTE UN PUNTO!");
   } else {
     // POSIBILIDAD DE AGREGAR EN UNA SOLA FUNCION
+    ////// AGREGAR POP DE MEME
     if (arrPokemonQuestion.length > currentQuestionIndex + 1) {
       nextButton.classList.remove("d-none");
     document.getElementById(`img${currentQuestionIndex}`).classList.remove("blackImage")
@@ -72,25 +69,25 @@ function selectAnswer(answerSelected) {
     } else {
       startButton.innerText = "Restart";
       pictures.innerHTML = "";
+      answerButtonsElement.innerHTML = "";
       startButton.classList.remove("d-none");
     }
   }
 }
 const answersButtons = (chainPokemones, rtaCorrecta) => {
-  // console.log("BOTONES", chainPokemones);
   console.log("ANS ", rtaCorrecta);
   chainPokemones.forEach((pokebutton) => {
-    // console.log(pokebutton);
     const button = document.createElement("button");
     button.innerText = pokebutton;
-
+    button.classList.add("btn")
+    button.classList.add("btn-warning")
+    button.classList.add("m-1")
     if (pokebutton == rtaCorrecta) {
       button.dataset.correct = true;
       button.id = "correcto";
     }
     button.addEventListener("click", () => {
       selectAnswer(button.id);
-      // console.log(button.id);
     });
     answerButtonsElement.appendChild(button);
   });
@@ -112,18 +109,10 @@ const pokeFalse = (pokemones, numeroPokemon) => {
   }
 };
 const pokemonCorrect = (pokemones, arrPokemonQuestion) => {
-  // console.log(arrPokemonQuestion);
   let numeroPokemon = arrPokemonQuestion[currentQuestionIndex];
-  // console.log(pokemones);
-  // console.log(currentQuestionIndex);
-  // console.log(numeroPokemon);
   let pokemonCorrect = pokemones[numeroPokemon - 1].name;
   chainPokemones.push(pokemonCorrect);
   rtaCorrecta = pokemonCorrect;
-  // console.log("Rta Correcta", chainPokemones);
-  // console.log("Rta Correcta", rtaCorrecta);
-  // console.log(numeroPokemon);
-  // pictures.innerHTML = `<img class="active" src="${fotoPokemon}" alt="pokemon">`;
   pokeFalse(pokemones, numeroPokemon);
 };
 const imgCharge = (arrPokemonQuestion) => {
@@ -132,18 +121,18 @@ const imgCharge = (arrPokemonQuestion) => {
     let numberFormateado = numberImgQuestion.toString().padStart(3, "0"); //padStart rellena la cadena con "0" si el lenght es menor a 3
     const fotoPokemon = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${numberFormateado}.png`;
     // console.log(fotoPokemon);
-    pictures.innerHTML += `<img class="active d-none blackImage" id= "img${imgID}" src="${fotoPokemon}" alt="pokemon">`;
+    pictures.innerHTML += `<img class="active d-none blackImage img-pokemon" id= "img${imgID}" src="${fotoPokemon}" alt="pokemon">`;
     console.log(medals);
     console.log(linkMedals[imgID]);
-    medals.innerHTML += `<img class="active blackImage" id= "medal${imgID}" src="${linkMedals[imgID]}" alt="medal1">`;
+    medals.innerHTML += `<img class="active blackImage gap-4 m-2 img-medal p-1 border border-black border-3" id= "medal${imgID}" src="${linkMedals[imgID]}" alt="medal1">`;
 
     imgID += 1;
   });
+  medals.classList.remove("d-none");
   document.getElementById("img0").classList.remove("d-none");
 };
 
 const arrPokemonQuestionFiller = (pokemones) => {
-  // console.log(arrPokemonQuestion);
   if (arrPokemonQuestion.length < 10) {
     arrPokemonQuestion.push(number());
     arrPokemonQuestionFiller(pokemones);
@@ -159,7 +148,6 @@ const obteniendoPokemons = () => {
     .get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=151`)
     .then((res) => {
       pokemones = res.data.results;
-      // console.log(pokemones);
       arrPokemonQuestionFiller(pokemones);
     })
     .catch((err) => console.error(err));
@@ -168,6 +156,7 @@ const obteniendoPokemons = () => {
 //en el click de next debo hacer un if que si el currentQuestionIndex = 9 se detenga y muestre el resultado
 const startGame = () => {
   startButton.classList.add("d-none");
+  quest.classList.remove("d-none");
   localStorage.setItem("Puntuacion", "0");
   currentQuestionIndex = 0;
   arrPokemonQuestion = [];
@@ -192,7 +181,6 @@ const mostrarOcultarImgId = (currentQuestionIndex) => {
 function setNextQuestion() {
   resetState();
   mostrarOcultarImgId(currentQuestionIndex);
-  // console.log(pokemones, arrPokemonQuestion);
   pokemonCorrect(pokemones, arrPokemonQuestion);
 }
 
